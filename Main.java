6fb0b5f6944c960e9d7536b1b9ac0d3a2228c868
francisco <franciscoballesta4@gmail.com>
2022-03-10 12:52:22 +0100

    cambio

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ class Main{
         
         public static void main(String[] args){
                 System.out.println("Hola mundo, desde java");
-                System.out.println("La conexion se ha echo");
+                System.out.println("La conexion se ha realizado");
         }
 }
